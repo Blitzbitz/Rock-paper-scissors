e4d4265f3c580bdbd75c1cc853a41010d1951433
Blitzbitz <165694529+Blitzbitz@users.noreply.github.com>
2024-08-07 15:09:04 +0100

    playRound function compares results from getHumanChoice and getComputerChoice needs work

--- myscripts.js
+++ myscripts.js
@@ -1,26 +1,42 @@
 
-function getComputerChoice(max){
-  
-  if( Math.floor(Math.random() * max) == 0 ){console.log("Rock")
 
-  }else if( Math.floor(Math.random() * max)>= 1){console.log("Paper")
 
-  }else if( Math.floor(Math.random() * max) >= 2){console.log("Scissors")
-return;
-}}
-getComputerChoice(3);
 
 
 
 function getHumanChoice(){
     let humanChoice = prompt("Please choose Rock, Paper, Scissors","Make a choice!");
-    if(humanChoice.toLowerCase() === ("rock")){console.log("Rock")
+    if(humanChoice.toLowerCase() === ("" || null )){getHumanChoice()
+
+    }else if(humanChoice.toLowerCase() === ("rock")){console.log("Rock")
 
     }else if (humanChoice.toLowerCase() === ("paper")){console.log("Paper")
     
     }else if (humanChoice.toLowerCase() === ("scissors")){console.log("Scissors")
-
-    }else{getHumanChoice()}
+    }else {getHumanChoice()}
 
 }
-getHumanChoice();
+function getComputerChoice(){
+  
+    if( Math.floor(Math.random() * 3) == 0 ){console.log("Rock")
+  
+    }else if( Math.floor(Math.random() * 3)<= 1){console.log("Paper")
+  
+    }else if( Math.floor(Math.random() * 3) == 2){console.log("Scissors")
+  }}
+//getHumanChoice();
+//getComputerChoice(3);
+let humanScore = 0;
+let computerScore = 0;
+let aHumanChoice ;
+let aComputerChoice ;
+
+playRound(getHumanChoice(),getComputerChoice(3));
+function playRound(aHumanChoice,aComputerCoice){
+   if(aHumanChoice === "Rock" && aComputerChoice !== ("Paper"|| "Rock")){console.log(`You win, Rock beats Scissors`)
+}else if(aHumanChoice  === "Paper" && aComputerChoice !== ("Scissors"|| "Paper")){console.log(`You win, Paper beats Scissors`)
+}else if(aHumanChoice  === "Scissors" && aComputerCoice !== ("Scissors" || "Rock")){console.log(`You win, Scissors beats Paper`)
+}else if (aHumanChoice === aComputerChoice){
+    console.log("draw")
+}else {console.log(`You lose`)}
+}
