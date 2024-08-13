3e318428b60896a3c24f537b7274cbd97c9bf035
Blitzbitz <165694529+Blitzbitz@users.noreply.github.com>
2024-08-13 13:44:42 +0100

    save before rewriting playRound function

--- myscripts.js
+++ myscripts.js
@@ -2,18 +2,22 @@
 let rock;
 let paper;
 let scissors; 
-
+let computerChoice;
+let humanScore = 0;
+let computerScore = 0;
+let aHumanChoice ;
+let aComputerChoice ;
 
 
 function getHumanChoice(){
     let humanChoice = prompt("Please choose Rock, Paper, Scissors","Make a choice!");
     if(humanChoice.toLowerCase() === ("")){getHumanChoice()
 
-    }else if(humanChoice.toLowerCase() === ("rock")){console.log("Rock");rock = 0;
+    }else if(humanChoice.toLowerCase() === ("rock")){console.log("Rock");rock = 0;aHumanChoice = rock;
 
-    }else if (humanChoice.toLowerCase() === ("paper")){console.log("Paper"); paper = 1;
+    }else if (humanChoice.toLowerCase() === ("paper")){console.log("Paper"); paper = 1;aHumanChoice = paper;
     
-    }else if (humanChoice.toLowerCase() === ("scissors")){console.log("Scissors"); scissors = 2 ;
+    }else if (humanChoice.toLowerCase() === ("scissors")){console.log("Scissors"); scissors = 2 ;aHumanChoice = scissors;
     }else if(humanChoice === null) {getHumanChoice()
     
     }else {getHumanChoice()}
@@ -21,26 +25,24 @@ function getHumanChoice(){
 }
 function getComputerChoice(){
   
-    if( Math.floor(Math.random() * 3) === 0 ){console.log("Rock")
+    if( Math.floor(Math.random() * 3) === 0 ){console.log("Rock");
+    acomputerChoice = 0;
   
-    }else if( Math.floor(Math.random() * 3)===1){console.log("Paper")
+    }else if( Math.floor(Math.random() * 3)===1){console.log("Paper");acomputerChoice = 1;
   
-    }else if( Math.floor(Math.random() * 3) === 2){console.log("Scissors")
+    }else if ( Math.floor(Math.random() * 3) === 2){console.log("Scissors");acomputerChoice = 2;
   }}
 //getHumanChoice();
-getComputerChoice(3);
-let humanScore = 0;
-let computerScore = 0;
-let aHumanChoice ;
-let aComputerChoice ;
+//etComputerChoice(3);
 
-playRound(getHumanChoice(),getComputerChoice(3));
+
 function playRound(aHumanChoice,aComputerChoice){
-   if(aHumanChoice === "Rock" && (aComputerChoice !== ("Paper"|| "Rock"))){console.log(`You win, Rock beats Scissors`)
-    }else if(aHumanChoice  === "Paper" && (aComputerChoice !== ("Scissors"|| "Paper"))){console.log(`You win, Paper beats Scissors`)
-    }else if(aHumanChoice  === "Scissors" && (aComputerCoice !== ("Scissors" || "Rock"))){console.log(`You win, Scissors beats Paper`)
+   if(aHumanChoice === rock || (aComputerChoice === (paper/*|| scissors*/))){console.log(`You win, Rock beats Scissors`)
+    }else if(aHumanChoice  === paper || (aComputerChoice === (scissors /*|| paper*/))){console.log(`You win, Paper beats Rock`)
+    }else if(aHumanChoice  === scissors || (aComputerChoice === (rock /*|| rock*/))){console.log(`You win, Scissors beats Paper`)
     }
     else if (aHumanChoice === aComputerChoice){
         console.log("draw")
     }else {console.log(`You lose`)}
     }
+    playRound(getHumanChoice(),getComputerChoice());
