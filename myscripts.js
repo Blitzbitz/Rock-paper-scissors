99a27efd43ac4f2a00ccee46cbe6a954055aad21
Blitzbitz <165694529+Blitzbitz@users.noreply.github.com>
2024-09-27 02:00:45 +0100

    game runs 5 rounds with results and console message

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,9 @@
+function playGame(){
 
 const rock = "rock";
 const paper ="paper";
 const scissors = "scissors"; 
-let computerChoice;
+let gameCount = 0;
 let humanScore = 0;
 let computerScore = 0;
 let aHumanChoice = getHumanChoice();
@@ -13,11 +14,11 @@ function getHumanChoice(){
     let aHumanChoice = prompt("Please type Rock, Paper or Scissors","");
     if(aHumanChoice === ("")){getHumanChoice()
 
-    }else if(aHumanChoice.toLowerCase() === ("rock")){aHumanChoice = rock;return(aHumanChoice);
+    }else if(aHumanChoice.toLowerCase() === ("rock")){aHumanChoice = rock;console.log(aHumanChoice);return(aHumanChoice);
 
-    }else if (aHumanChoice.toLowerCase() === ("paper")){ aHumanChoice = paper ;return(aHumanChoice);
+    }else if (aHumanChoice.toLowerCase() === ("paper")){ aHumanChoice = paper ;console.log(aHumanChoice);return(aHumanChoice);
     
-    }else if (aHumanChoice.toLowerCase() === ("scissors")){aHumanChoice = scissors;return(aHumanChoice);
+    }else if (aHumanChoice.toLowerCase() === ("scissors")){aHumanChoice = scissors;console.log(aHumanChoice);return(aHumanChoice);
     }else if(aHumanChoice === null) {getHumanChoice()
     
     }else if (aHumanChoice !== "rock" || "paper" ||"scissors"){getHumanChoice()
@@ -29,14 +30,16 @@ function getHumanChoice(){
 
 function getComputerChoice(){
   
-    if( Math.floor(Math.random() * 3) === 0 ){
-    aComputerChoice = "rock";return(aComputerChoice);
+    if( Math.round(Math.random() * 3) === 0 ){
+        aComputerChoice = "rock";console.log(aComputerChoice);return(aComputerChoice);
 
-    }else if( Math.floor(Math.random() * 3)===1){aComputerChoice = "paper";return(aComputerChoice);
+    }else if( Math.round(Math.random() * 3)===1){
+        aComputerChoice = "paper";console.log(aComputerChoice);return(aComputerChoice);
   
-    }else if ( Math.floor(Math.random() * 3) === 2){aComputerChoice = "scissors";
-    return(aComputerChoice)    
-  }else{getComputerChoice(3)}}
+    }else if ( Math.round(Math.random() * 3) === 2){
+        aComputerChoice = "scissors";console.log(aComputerChoice);
+        return(aComputerChoice)    
+    }else{getComputerChoice(3)}}
 
 //getHumanChoice()
 getComputerChoice(3);
@@ -45,12 +48,24 @@ console.log(aComputerChoice);
 
 
 function playRound(){
-   if(aHumanChoice === "rock" && (aComputerChoice === "scissors")){console.log(`You win, Rock beats Scissors`)
-    }else if(aHumanChoice  === "paper" && (aComputerChoice === "rock")){console.log(`You win, Paper beats Rock`)
-    }else if(aHumanChoice  === "scissors" && (aComputerChoice === "paper")){console.log(`You win, Scissors beats Paper`)
+   if(aHumanChoice === "rock" && (aComputerChoice === "scissors")){console.log(`You win`);humanScore++;gameCount++;
+    }else if(aHumanChoice  === "paper" && (aComputerChoice === "rock")){console.log(`You win`);humanScore++;gameCount++;
+    }else if(aHumanChoice  === "scissors" && (aComputerChoice === "paper")){console.log(`You win`);humanScore++;gameCount++;
     }
     else if (aHumanChoice === aComputerChoice){
-        console.log("draw")
-    }else {console.log(`You lose`)}
+        console.log("draw");gameCount++;
+    }else {console.log(`You lose`);computerScore++;gameCount++;}
+    console.log(gameCount)
     }
-    playRound(aHumanChoice,aComputerChoice); 
+     
+     
+     
+     for (gameCount = 0 ; gameCount < 5;){aHumanChoice = getHumanChoice();getComputerChoice(3);playRound(aHumanChoice,aComputerChoice)};
+    if (gameCount = 5 && humanScore > computerScore){console.log(`Well done, You scored ${humanScore}, and the computer scored ${computerScore}.
+    You won!`)
+    }else if(gameCount = 5 && computerScore > humanScore){console.log(`Game over, You scored ${humanScore}, The computer scored ${computerScore} You lost`)}
+    else{console.log(`Game over, You scored ${humanScore}, The computer scored ${computerScore}`)}
+
+}
+playGame();
+
